53d10863091105cb287158fe9e47d20497e610fb
Wilfer Zuluaga <wdzuluaga54@misena.edu.co>
2024-09-18 12:49:19 -0500

    hacky sack

--- client/src/views/Dashboard/index.jsx
+++ client/src/views/Dashboard/index.jsx
@@ -10,26 +10,37 @@ function Dashboard() {
         </div>
         <div className="user-menu">
           <span className="user-name">Bienvenido</span>
-          <div className="dropdown">
-            <Link to="/login" className="dropbtn">Login</Link>
-            <div className="dropdown-content">
-              <Link to="/register" id="logout">
-                Cerrar sesión
+          <div className="user-access">
+            <div className="dropdown">
+              <Link to="/login" className="dropbtn">
+                Login
               </Link>
+              <div className="dropdown-content">
+                <Link to="/register" id="logout">
+                  Cerrar sesión
+                </Link>
+              </div>
             </div>
+            <Link to="/register" className="dropbtn">
+              Registrarse
+            </Link>
           </div>
         </div>
       </header>
 
       <div className="dashboard-grid">
         <div className="card" id="productsCard">
-          <Link to="/products"><h3>Productos</h3></Link>
+          <Link to="/products">
+            <h3>Productos</h3>
+          </Link>
         </div>
         <div className="card" id="providersCard">
           <h3>Proveedores</h3>
         </div>
         <div className="card" id="categoriesCard">
-          <Link to='/categories'><h3>Categorías</h3></Link>
+          <Link to="/categories">
+            <h3>Categorías</h3>
+          </Link>
         </div>
         <div className="card" id="agendasCard">
           <h3>Agendas</h3>
